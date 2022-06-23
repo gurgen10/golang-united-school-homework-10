1a91db3a3abc57de212975e140747168763d7297
gurgen11 <gurgen@retailmerchantgroup.com>
2022-06-23 22:43:41 +0400

    Web Programming

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -20,11 +21,56 @@ main function reads host/port from env just for an example, flavor it following 
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
+	router.HandleFunc("/name/{PARAM}", NameHandler).Methods("Get")
+	router.HandleFunc("/bad", BadHandler).Methods("Get")
+	router.HandleFunc("/data", DataHandler).Methods("Post")
+	router.HandleFunc("/header", HeaderHandler).Methods("Get")
+	http.Handle("/", router)
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
 		log.Fatal(err)
 	}
+}
+func NameHandler(w http.ResponseWriter, r *http.Request) {
+	param := mux.Vars(r)["PARAM"]
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello, %s!", param)
+
+}
+func BadHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	fmt.Fprintf(w, "%v", http.StatusInternalServerError)
+
+}
+func DataHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(string(body))
+
+	w.WriteHeader(http.StatusOK)
+	if len(body) > 0 {
+		fmt.Fprintf(w, "`I got message:\n%v`", string(body))
+	}
+
+}
+func HeaderHandler(w http.ResponseWriter, r *http.Request) {
+	a := r.Header.Get("a")
+	b := r.Header.Get("b")
+
+	log.Printf("Headers: a: %s, b: %s", a, b)
+	aVal, err := strconv.Atoi(a)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bVal, err1 := strconv.Atoi(b)
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+
+	w.Header().Set("a+b", strconv.Itoa(aVal+bVal))
 }
 
 //main /** starts program, gets HOST:PORT param and calls Start func.
